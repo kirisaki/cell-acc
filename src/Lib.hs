3a6f5b5bd1fb249abdb88068efea60a44d5635e9
Akihito KIRISAKI <kirisaki@klaraworks.net>
2019-05-02 15:56:15 +0900

    Exec example

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,12 @@
 module Lib
-    ( someFunc
+    ( entry
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Array.Accelerate              as A
+import Data.Array.Accelerate.LLVM.Native  as CPU
+
+dotp :: Acc (Vector Float) -> Acc (Vector Float) -> Acc (Scalar Float)
+dotp xs ys = A.fold (+) 0 (A.zipWith (*) xs ys)
+
+entry :: IO ()
+entry = print . CPU.run $ dotp (use $ fromList (Z:.10) [0..] ) (use $ fromList (Z:.10) [1,3..] )
