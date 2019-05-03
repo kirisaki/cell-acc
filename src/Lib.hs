046be7499a6fc51e3cfda3cc9787f927481a7f84
Akihito KIRISAKI <kirisaki@klaraworks.net>
2019-05-03 11:28:27 +0900

    display bitmap

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,9 +4,21 @@ module Lib
 
 import Data.Array.Accelerate              as A
 import Data.Array.Accelerate.LLVM.Native  as CPU
+import Graphics.Gloss
+import Graphics.Gloss.Accelerate.Data.Picture
+import Data.Word
 
-dotp :: Acc (Vector Float) -> Acc (Vector Float) -> Acc (Scalar Float)
+window :: Display
+window = FullScreen
+
+dotp :: Acc (A.Vector Float) -> Acc (A.Vector Float) -> Acc (Scalar Float)
 dotp xs ys = A.fold (+) 0 (A.zipWith (*) xs ys)
 
+board :: Matrix Word32
+board = fromList (Z:.600:.256) (fmap Prelude.fromIntegral [0xff000000..])
+
 entry :: IO ()
-entry = print . CPU.run $ dotp (use $ fromList (Z:.10) [0..] ) (use $ fromList (Z:.10) [1,3..] )
+entry = do
+  display window white $ bitmapOfArray board True
+--entry = display window white (text "Hello, world!!!")
+--entry = print . CPU.run $ dotp (use $ fromList (Z:.100000000) [0..] ) (use $ fromList (Z:.100000000) [1,3..] )
